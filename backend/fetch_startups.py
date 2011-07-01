9466397d7be8815e16d4873333c5fe5e55f63b5c
Richard Brooks <rich90usa@gmail.com>
2011-07-01 18:03:46 -0400

    Minimized the size of the startups.js file for improved page loading time.

--- backend/fetch_startups.py
+++ backend/fetch_startups.py
@@ -6,14 +6,6 @@ import os
 
 CRUNCHBASE_COMPANY_API_URL = 'http://api.crunchbase.com/v/1/company/'
 
-class Company:
-    def __init__(self, name, offices, number_of_employees, homepage_url, permalink):
-        self.name = name
-        self.offices = offices
-        self.number_of_employees = number_of_employees
-        self.homepage_url = homepage_url
-        self.permalink = permalink
-
 print "Script starting..."
 file = open('./startups.js', 'r')
 
@@ -35,13 +27,20 @@ for company in companies:
  else:
     companies_detailed.append(result)
 
-""" NOT WORKING NOT SERIALIZING TO JSON
-#Distill details down to essentials
+""" NOT WORKING NOT SERIALIZING TO JSON """
 companies_distilled = []
 for company in companies_detailed:
-    temp_company = Company(company['name'], company['offices'], company['number_of_employees'], company['homepage_url'], company['homepage_url'])
+    temp_company = {}
+    temp_company['name'] = company['name']
+    temp_company['offices'] = company['offices']
+    temp_company['permalink'] = company['permalink']
+    temp_company['number_of_employees'] = company['number_of_employees']
+    temp_company['homepage_url'] = company['homepage_url']
     companies_distilled.append(temp_company)
-"""
+
+file = open('./startups_distilled.js', 'w')
+file.write(json.dumps(companies_distilled))
+file.close
 
 file = open('./startups_detailed.js', 'w')
 file.write(json.dumps(companies_detailed))
